edb504487bc227af7e37e0b67bfaa35e4342065d
Mopidevi Sumanth Teja <msteja1002@gmail.com>
2026-07-19 18:59:11 +0530

    Updated day 5 codes

---
+++ D5datatypes.py
@@ -0,0 +1,43 @@
+set1=(1,2,3,4,5,1,2,3,4,5)
+print(set1)
+
+print(hash(1))
+#unordered:
+names=['Umamahesh','sumanth','Venketash']
+print(names)
+
+print(hash('umamahesh'))
+#note: hash() is the function we use to know the hash value of an object.
+
+
+details=['umamahesh','28','Bangalore','sales man']
+details={'name':'umamahesh', 'age':28, 'city':'Bangalore', 'occupation':'sales man'}
+print(details)
+
+# we can create an frozenset by passing any sequence to the feozenset() func.
+list1=[1,2,3,4,5,6]
+fs1=frozenset(list1)
+print(fs1)
+
+#object:
+num1=10
+#values
+print(num1)
+#type
+print (type(num1))
+#id
+print(id(num1))
+
+
+list1=['python','java','Data Science','Ai']
+print(list)
+#type
+print(type(list1))
+#id
+print(id(list1))
+
+def addition():
+   a=10
+   b=4
+  a+b
+ print(addition())
